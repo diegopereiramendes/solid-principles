6dde8fec5b110aa546904d1570241931d0c9d41b
Diego Mendes <diego@jaya.tech>
2020-03-27 10:46:07 -0300

    finalizado estudo principio open closed

---
+++ src/main/java/com/diegomendes/solid/principles/ii_open_closed/README.md
@@ -0,0 +1,6 @@
+**Open Closed Principle**
+- uma classe deve ser aberta para extensão, e fechada para alteração
+
+Cenário do código: *Imagine um cenário no qual você deseja calcular o preço de alguel para cada inquilino através 
+de uma tabela de preço, mas futuramente você também terá uma nova tabela de Preço Diferenciada para algunes clientes, a ideia é mostrar 
+o custo de desenvolvimento disso*
--- src/main/java/com/diegomendes/solid/principles/ii_open_closed/old_code/CalculadoraAluguel.java
+++ src/main/java/com/diegomendes/solid/principles/ii_open_closed/old_code/CalculadoraAluguel.java
@@ -3,7 +3,7 @@ package com.diegomendes.solid.principles.ii_open_closed.old_code;
 public class CalculadoraAluguel {
 
   public Double calcula(final Cliente cliente){
-    AluguelPadrao aluguelPadrao = new AluguelPadrao();
+    AluguelPadrao aluguelPadrao = new AluguelPadrao(); // esse `new` não tá legal, e se existisse outro tipo de alugel? teriamos que modificar essa classe calculadora para acrescentar outro metodo
 
     return aluguelPadrao.valorAluguel(cliente);
   }
